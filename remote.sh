e1014f525f16ddc84c008dde716e91065eb78705
Douglas Beal <git@dougbeal.com>
2016-04-29 11:35:14 -0700

    added fontawesome

--- remote.sh
+++ remote.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 git remote add css https://github.com/foolscapcon/startbootstrap-business-frontpage.git
 git remote add bootstrap-sass https://github.com/foolscapcon/bootstrap-sass.git
+git remote add font-awesome-sass https://github.com/foolscapcon/font-awesome-sass.git
+git fetch css
+git fetch bootstrap-sass
+git fetch font-awesome-sass
